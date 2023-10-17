778e668077cd191903f6854c94ccbe6a10782dd3
AdarshPNair <adarshpnair662@gmail.com>
2023-10-18 00:19:38 +0530

    added bookmark button that will show along the youtube play button

---
+++ contentScript.js
@@ -0,0 +1,34 @@
+(()=>{
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+   chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {type, value, videoId} = obj;
+
+        if(type === "NEW")
+        {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+   });
+   
+   const newVideoLoaded = () => {
+     const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+     if(!bookmarkBtnExists){
+
+      const bookmarkBtn = document.createElement("img");
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png")
+      bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+      bookmarkBtn.title = "add to bookmark"
+
+     youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+     youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+     youtubeLeftControls.appendChild(bookmarkBtn);
+     }
+   }
+
+   newVideoLoaded();
+
+   console.log("test");
+
+})();
