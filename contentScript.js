f52d0364541664c62760597d569140751988bc76
AdarshPNair <adarshpnair662@gmail.com>
2023-10-18 10:59:12 +0530

    bug fix: fixed bookmarker button repeated coming when opening a new video from same page

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,27 @@
 (()=>{
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
+//     let counter = 0;
    chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const {type, value, videoId} = obj;
+
+       
 
         if(type === "NEW")
         {
             currentVideo = videoId;
-            newVideoLoaded();
+             newVideoLoaded();
         }
    });
    
    const newVideoLoaded = () => {
      const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+     console.log(bookmarkBtnExists);
      if(!bookmarkBtnExists){
 
       const bookmarkBtn = document.createElement("img");
       bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png")
-      bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
       bookmarkBtn.title = "add to bookmark"
 
      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
@@ -26,9 +30,6 @@
      youtubeLeftControls.appendChild(bookmarkBtn);
      }
    }
-
-   newVideoLoaded();
-
-   console.log("test");
+newVideoLoaded();
 
 })();
